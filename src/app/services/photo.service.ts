dd115c18c4c8603dbb10e05f20cd4667cfd70459
AntonioVillegas <antonio.villegas@epn.edu.ec>
2022-11-27 12:18:42 -0500

    cambios actualizas

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -8,18 +8,24 @@ import { Preferences } from '@capacitor/preferences';
   providedIn: 'root'
 })
 export class PhotoService {
-
+  public photos: UserPhoto[] = [];
   constructor() { }
 
 
   public async addNewToGallery() {
     // Take a photo
     const capturedPhoto = await Camera.getPhoto({
-      resultType: CameraResultType.Uri,
-      source: CameraSource.Camera,
-      quality: 100
-    });
-  }
+    resultType: CameraResultType.Uri,
+    source: CameraSource.Camera,
+    quality: 100
+  });
+
+  this.photos.unshift({
+    filepath: "soon...",
+    webviewPath: capturedPhoto.webPath
+  });
+}
+  
 
   
 
